819d44ecf3c784d20dfe6bea01f2ec3974d5dda6
Gadiza Zerari <gadizaz@hotmail.com>
2018-03-21 15:49:24 +0000

    Added subject in dockingstation.spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,7 +3,7 @@ require "docking_station"
 describe DockingStation do
 #  it {is_expected.to respond_to (:release_bike) }
   it "releases a working bike" do
-    bike = DockingStation.new.release_bike
+    bike = subject.release_bike
     expect(bike.working?).to eq(true)
   end
 end
